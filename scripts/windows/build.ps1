fd0d66fe4641c1fa300ec8affda1aa44a1b8f0c5
Ross Kirkpatrick <ross.kirkpatrick@suse.com>
2022-04-15 02:00:17 -0400

    jamie to the rescue

--- scripts/windows/build.ps1
+++ scripts/windows/build.ps1
@@ -30,13 +30,13 @@ function Build {
 
     if ($env:DEBUG) {
         $Env:GO_LDFLAGS = '-v -gcflags=all=-N -l'
-        Write-Host ('Debug flag passed, changing ldflags to {0}' -f $linkFlags)
+        Write-LogInfo ('Debug flag passed, changing ldflags to {0}' -f $linkFlags)
         # go install github.com/go-delve/delve/cmd/dlv@latest
     }
 
     $GO_LDFLAGS = ("'{0} -X {1}/{2}/api/v3/version.GitSHA={3}'" -f $Env:GO_LDFLAGS, $env:GIT_ORG, $env:GIT_REPO, $Commit)
     if ($env:DEBUG){
-        Write-Host "[DEBUG] Running command: go build -o $Output -ldflags $linkerFlags"
+        Write-LogInfo "[DEBUG] Running command: go build -o $Output -ldflags $linkerFlags"
     }
 
     Push-Location $BuildPath
@@ -60,16 +60,18 @@ Invoke-Script -File "$PSScriptRoot\version.ps1"
 $SRC_PATH = (Resolve-Path "$PSScriptRoot\..\..").Path
 Push-Location $SRC_PATH
 if ($env:DEBUG) {
-    Write-Host "[DEBUG] Build Path: $SRC_PATH"
+    Write-LogInfo "[DEBUG] Build Path: $SRC_PATH"
 }
 
-# Remove-Item -Path "$SRC_PATH\bin\*.exe" -Force -ErrorAction Ignore
+Remove-Item -Path "$SRC_PATH/bin/*.exe" -Force -ErrorAction Ignore
 $null = New-Item -Type Directory -Path bin -ErrorAction Ignore
 $env:GOARCH = $env:ARCH
 $env:GOOS = 'windows'
 $env:CGO_ENABLED = 0
 
+Write-LogInfo "Starting Builds for etcd and etcdctl"
 Build -BuildPath "$SRC_PATH/server" -Commit $env:COMMIT -Output "..\bin\etcd.exe" # -Version $env:VERSION
 Build -BuildPath "$SRC_PATH/etcdctl" -Commit $env:COMMIT -Output "..\bin\etcdctl.exe" # -Version $env:VERSION
+Write-LogInfo "Builds Complete"
 
 Pop-Location
